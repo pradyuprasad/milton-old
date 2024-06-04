f88532e3df0ffc0a889140aaef6ed4b6e0d2e451
pradyuprasad <pradyu.sp@gmail.com>
2024-06-04 19:38:10 +0800

    wrote database selector

--- website/backend/app.py
+++ website/backend/app.py
@@ -2,6 +2,8 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from ...ai_tools.database_selector import write_dataset_picking_instructions, write_SQL_query
+
 app = FastAPI()
 
 
@@ -12,3 +14,7 @@ async def root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
+
+@app.post("/data_selector")
+def get_dataset():
+    return None
